4e7cf6864675a689f0b46572dc0eb829ea3dcaf5
HIMANSHU PATHAK <111076074+himanshupa369@users.noreply.github.com>
2025-04-17 09:59:49 +0530

    Create Static_in_thread.cpp

---
+++ Multithreading/Static_in_thread.cpp
@@ -0,0 +1,28 @@
+// is static variable thread safe or not ??
+// Ans:- Static variable is not thread safe.
+
+//steps::- Fetch,modify,store.
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+static int s = 0;
+
+mutex m;
+void fun() {
+   int count = 100000;
+   m.lock();
+   while (count--) {
+       ++s;
+   }
+   m.unlock();
+   cout << s << endl;
+}
+
+int main() {
+   std::thread t1(fun);
+   std::thread t2(fun);
+   t1.join();
+   t2.join();
+}
