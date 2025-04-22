e273e46e98e46fd71a025eb62012eb63029b7bd7
HIMANSHU PATHAK <111076074+himanshupa369@users.noreply.github.com>
2025-04-22 16:58:51 +0530

    Update OpenClosedPrinciple.cpp

--- Multithreading/Solid Principle/OpenClosedPrinciple.cpp
+++ Multithreading/Solid Principle/OpenClosedPrinciple.cpp
@@ -3,6 +3,32 @@
 
 🧠 Definition:
 Software entities (classes, modules, functions, etc.) should be open for extension but closed for modification.
+(Definition: In object-oriented programming, the open/closed principle states that "software entities such as classes, modules, functions, etc. 
+should be open for extension, but closed for modification")
+
+1. Which means, any new functionality should be implemented by adding new classes, attributes and methods, instead of changing the current ones or existing ones.
+2. Bertrand Meyer is generally credited for having originated the term open/closed principle and This Principle is considered by Bob Martin as “the most important principle of object-oriented design”.
+
+Implementation guidelines
+1. The simplest way to apply OCP is to implement the new functionality on new derived (sub) classes that inherit the original class implementation.
+2. Another way is to allow client  to access the original class with an abstract interface, 
+3. So, at any given point of time when there is a requirement change instead of touching the existing functionality it’s always suggested to create new classes and leave the original implementation untouched.
+
+Pit falls of Not following OCP : What if I do not follow Open closed principle during a requirement enhancement in the development process. In that case, we end up with the following disadvantages
+1) If a class or a function always allows the addition of new logic, as a developer we end up testing the entire functionality along with the requirement.
+(End up testing the entire functionality)
+
+2) Also, as a developer we need to ensure to communicate the scope of the changes to the Quality Assurance team in advance so that they can gear up for enhanced regression testing along with the feature testing. 
+(QA team need to test entire flow.)
+
+3) Step 2 above is a costly process to adapt for any organization
+(Costly process for organization.)
+
+4) Not following the Open Closed Principle breaks the SRP since the class or function might end up doing multiple tasks.
+(Breaks the single responsibility as well)
+
+5) Also, if the changes are implemented on the same class, Maintenance of the class becomes difficult since the code of the class increases by thousands of unorganised lines. 
+(maintenance overheads increases on the classes.)
 
 That means:
 - You should be able to add new functionality
