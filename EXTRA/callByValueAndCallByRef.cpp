7c76019a893e549a3e39045ab699315ef3922dcc
HIMANSHU PATHAK <111076074+himanshupa369@users.noreply.github.com>
2025-06-02 09:39:21 +0530

    Update callByValueAndCallByRef.cpp

--- EXTRA/callByValueAndCallByRef.cpp
+++ EXTRA/callByValueAndCallByRef.cpp
@@ -3,3 +3,10 @@
 | `ObjectType`        | New object          | Yes (but optimized) | No                   | Creating new objects             |
 | `ObjectType&`       | Same as original    | No                  | Yes                  | Referring to existing members    |
 | `const ObjectType&` | Same as original    | No                  | No                   | Access existing object read-only |
+
+// used in smart pointer implementation.  
+| Expression              | Meaning                                                             |
+| ----------------------- | ------------------------------------------------------------------- |
+| `Integer* operator->()` | Overload `->` so your object can behave like a pointer to `Integer` |
+| Used in                 | Smart pointers, proxies, wrappers                                   |
+| Why?                    | Enable `obj->member` syntax on non-pointer objects                  |
