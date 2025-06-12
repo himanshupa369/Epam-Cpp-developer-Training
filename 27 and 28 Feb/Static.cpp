c67de0b5fc96eae32db01603518f71df4fb76284
HIMANSHU PATHAK <111076074+himanshupa369@users.noreply.github.com>
2025-06-12 21:04:22 +0530

    Update Static.cpp

--- 27 and 28 Feb/Static.cpp
+++ 27 and 28 Feb/Static.cpp
@@ -2,6 +2,32 @@
 
 //A static function belongs to the class rather than to an instance of the class.
 //It cannot access non - static members because it does not have a this pointer.
+
+#include<iostream>
+class test{
+    int val;
+    static int data;
+    public:
+    test(int val):val(val){}
+    
+    // normal non static member function can access both static and non static data member
+    static void staticfun(){
+        std::cout<<data<<std::endl;
+       // std::cout<<val<<std::endl; //error
+    }
+     void non_staticfun(){
+        std::cout<<data<<std::endl;
+        std::cout<<val<<std::endl; 
+    }
+};
+int test::data = 0;
+int main(){
+    test t(3);
+    test::staticfun();
+    t.non_staticfun();
+    return 0;
+}
+
 //It can only access static data members and static functions.
 //It can be called using the class name (ClassName::StaticFunction()), without creating an instance.
 
